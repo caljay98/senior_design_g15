4bf6d58cd73583e74d2b7437a67764c32426443f
sebasler <43249595+sebasler@users.noreply.github.com>
2023-04-20 22:55:55 -0500

    Fixed, buddy

--- Core/Src/user_input.c
+++ Core/Src/user_input.c
@@ -49,6 +49,9 @@ bool new_enc2_event = false;
 
 static bool handle_button(BUTTON_t* button);
 
+
+static volatile unsigned char state;
+static int32_t count = 0;
 void get_pending_input_events(INPUTS_t* inputs)
 {
 	inputs->back_click = handle_button(&back);
@@ -56,19 +59,18 @@ void get_pending_input_events(INPUTS_t* inputs)
 	inputs->mode_click = handle_button(&mode);
 	inputs->select_click = handle_button(&select);
 
-	if (new_enc1_event && new_enc2_event)
+	static int32_t lastCount = 0;
+	if (lastCount > count)
 	{
-		if (enc1_last > enc2_last)
-		{
-			inputs->left_spin = true;
-		}
-		else
-		{
-			inputs->right_spin = true;
-		}
-		new_enc1_event = false;
-		new_enc2_event = false;
-		last_spin = HAL_GetTick();
+		// Left spin
+		inputs->left_spin = true;
+		lastCount--;
+	}
+	else if (lastCount < count)
+	{
+		// Right spin
+		inputs->right_spin = true;
+		lastCount++;
 	}
 }
 
@@ -89,22 +91,86 @@ static bool handle_button(BUTTON_t* button)
 	return false;
 }
 
+
+
+// Values returned by 'process'
+// No complete step yet.
+#define DIR_NONE 0x0
+// Clockwise step.
+#define DIR_CW 0x10
+// Anti-clockwise step.
+#define DIR_CCW 0x20
+
+#define R_START 0x0
+
+#ifdef HALF_STEP
+// Use the half-step state table (emits a code at 00 and 11)
+#define R_CCW_BEGIN 0x1
+#define R_CW_BEGIN 0x2
+#define R_START_M 0x3
+#define R_CW_BEGIN_M 0x4
+#define R_CCW_BEGIN_M 0x5
+const unsigned char ttable[6][4] = {
+  // R_START (00)
+  {R_START_M,            R_CW_BEGIN,     R_CCW_BEGIN,  R_START},
+  // R_CCW_BEGIN
+  {R_START_M | DIR_CCW, R_START,        R_CCW_BEGIN,  R_START},
+  // R_CW_BEGIN
+  {R_START_M | DIR_CW,  R_CW_BEGIN,     R_START,      R_START},
+  // R_START_M (11)
+  {R_START_M,            R_CCW_BEGIN_M,  R_CW_BEGIN_M, R_START},
+  // R_CW_BEGIN_M
+  {R_START_M,            R_START_M,      R_CW_BEGIN_M, R_START | DIR_CW},
+  // R_CCW_BEGIN_M
+  {R_START_M,            R_CCW_BEGIN_M,  R_START_M,    R_START | DIR_CCW},
+};
+#else
+// Use the full-step state table (emits a code at 00 only)
+#define R_CW_FINAL 0x1
+#define R_CW_BEGIN 0x2
+#define R_CW_NEXT 0x3
+#define R_CCW_BEGIN 0x4
+#define R_CCW_FINAL 0x5
+#define R_CCW_NEXT 0x6
+
+const unsigned char ttable[7][4] = {
+  // R_START
+  {R_START,    R_CW_BEGIN,  R_CCW_BEGIN, R_START},
+  // R_CW_FINAL
+  {R_CW_NEXT,  R_START,     R_CW_FINAL,  R_START | DIR_CW},
+  // R_CW_BEGIN
+  {R_CW_NEXT,  R_CW_BEGIN,  R_START,     R_START},
+  // R_CW_NEXT
+  {R_CW_NEXT,  R_CW_BEGIN,  R_CW_FINAL,  R_START},
+  // R_CCW_BEGIN
+  {R_CCW_NEXT, R_START,     R_CCW_BEGIN, R_START},
+  // R_CCW_FINAL
+  {R_CCW_NEXT, R_CCW_FINAL, R_START,     R_START | DIR_CCW},
+  // R_CCW_NEXT
+  {R_CCW_NEXT, R_CCW_FINAL, R_CCW_BEGIN, R_START},
+};
+#endif
+
+
 // interrupt for the encoder
 // TODO this is not great, sometimes gives both
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-	if (HAL_GetTick() - last_spin <= TIME_BETWEEN_SPINS_ms) return;
+	// Grab state of input pins.
+	  unsigned char pinstate = (HAL_GPIO_ReadPin(ENC_1_GPIO_Port, ENC_1_Pin) << 1) | HAL_GPIO_ReadPin(ENC_2_GPIO_Port, ENC_2_Pin);
+	  // Determine new state from the pins and state table.
+	  state = ttable[state & 0xf][pinstate];
+	  // Return emit bits, ie the generated event.
+	  unsigned char result = state & 0x30;
 
-	if (GPIO_Pin == ENC_1_Pin && !new_enc1_event)
-	{
-		enc1_last = HAL_GetTick();
-		new_enc1_event = true;
-	}
-	if (GPIO_Pin == ENC_2_Pin && !new_enc2_event)
-	{
-		enc2_last = HAL_GetTick();
-		new_enc2_event = true;
-	}
+	  if (result == DIR_CW)
+	  {
+		  count++;
+	  }
+	  else if (result == DIR_CCW)
+	  {
+		  count--;
+	  }
 }
 
 
